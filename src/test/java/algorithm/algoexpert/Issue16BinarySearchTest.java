581d851b79d09718e3328936d4344572683e024f
Miguel Cortes <miguel.cortes@ias.com.co>
2024-01-13 18:44:08 -0500

    abstract array with method setup

--- src/test/java/algorithm/algoexpert/Issue16BinarySearchTest.java
+++ src/test/java/algorithm/algoexpert/Issue16BinarySearchTest.java
@@ -1,14 +1,21 @@
 package algorithm.algoexpert;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Issue16BinarySearchTest {
 
+    private int[] array;
+
+    @BeforeEach
+    void setUp() {
+        array = new int[]{0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
+    }
+
     @Test
     void binarySearch1() {
-        int[] array = {0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
         int target = 33;
         int expected = 3;
         int actual = Issue16BinarySearch.binarySearch1(array, target);
@@ -17,7 +24,6 @@ class Issue16BinarySearchTest {
 
     @Test
     void binarySearch2() {
-        int[] array = {0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
         int target = 33;
         int expected = 3;
         int actual = Issue16BinarySearch.binarySearch2(array, target);
